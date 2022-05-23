937c4e39c05ae43e111b582b14a66f2d85edbcdd
kinsanghun <52707244+kinsanghun@users.noreply.github.com>
2022-05-23 20:32:23 +0900

    dd

--- main/views.py
+++ main/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .comparealgorithm import compare_model
 # Create your views here.
 
+def compare_time(start, end):
+    s = str(start).split(":")
+    e = str(end).split(":")
+    res = ((int(s[0]) * 60) + int(s[1])) - ((int(e[0]) * 60) + int(e[1]))
+    print(res)
+    return res
 def index(request):
     pid = request.GET.get("pid", "0")
     start = request.GET.get("start", "")
@@ -10,12 +16,24 @@ def index(request):
     ensure_type = request.GET.get("ensure_type", "")
     driving_km = request.GET.get("driving_km", "")
     rental_day = request.GET.get("rental_day", "")
-
+    print(start,end)
     error = ""
     greencar = list()
     socar = list()
 
     if int(pid):
+        if compare_time(start, end) > 0 :
+            context = {
+                'start' : start,
+                'end' : end,
+                'car_type': car_type,
+                'ensure_type' : ensure_type,
+                'driving_km' : driving_km,
+                'rental_day' : rental_day,
+                'error' : "대여시작시간이 대여종료시간보다 빠릅니다."
+            }
+            return render(request, "index.html", context)
+
         if car_type == "중형":
             error = "쏘카는 대여할 수 있는 중형 차량 없음!"
 
@@ -33,7 +51,6 @@ def index(request):
                 socar.append(result[1]["car"][i])
                 socar.append(int(result[1]["totalpay"][i]))
                 socar.append(result[1]["coupon"][i])
-
 
     context = {
         'start' : start,
